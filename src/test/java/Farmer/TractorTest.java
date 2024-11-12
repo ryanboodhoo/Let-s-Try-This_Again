463ab3d225e37e1a473a6ce66165fa0b0660794e
Alexandria Paul <avpaul527@gmail.com>
2024-11-12 11:39:25 -0500

    test cases

--- src/test/java/Farmer/TractorTest.java
+++ src/test/java/Farmer/TractorTest.java
@@ -1,12 +1,19 @@
 package Farmer;
 
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.junit.platform.commons.util.ReflectionUtils;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class TractorTest {
+    Tractor tractor;
 
     @Test
-    void harvest() {
+    void testHarvest() {
+        CropRow cropRow = new CropRow();
+
+        //Can tractor harvest?
+        Tractor.harvest(cropRow);
     }
 }
